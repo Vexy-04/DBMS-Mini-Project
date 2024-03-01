d579064cf322130621f72282eb9e2e9de81fb68a
Vexy-04 <varunal2003@gmail.com>
2024-03-02 00:49:12 +0530

    header css

--- assest/header.php
+++ assest/header.php
@@ -1,7 +1,7 @@
 <!-- <header class="blog-header border-bottom shadow-sm"> -->
 <header class="blog-header bhead">
     <link rel="stylesheet" href="css/header.css">
-    <div class="container-fluid" style="padding-left: 3rem; padding-right:3rem">
+    <div class="container-fluid bgclr" style="padding-left: 3rem; padding-right:3rem">
         <div class="d-flex flex-column flex-md-row align-items-center py-2">
             <a href="index.php" class="my-0 mr-md-auto" style="width: 6rem;">
                 <!-- <img src="img/logo/logo.png" alt="dev culture logo" style="width: 80%;height: auto;"> -->
@@ -10,7 +10,7 @@
 
             <?php if ($loggedin) : ?>
 
-                <nav class="my-2 my-md-0 mr-md-3 txtclr">
+                <nav class="my-2 my-md-0 mr-md-3">
                     <a class="p-2 px-5 text-muted" href="index.php">Home</a>
                     <a class="p-2 px-5 text-muted" href="categories.php">Category</a>
                     <a class="p-2 px-5 text-muted" href="article.php">Article</a>
