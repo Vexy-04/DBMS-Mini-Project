901856af1d24144b464309327fb9e1392b54a50f
2003anjali <anjali.nambiar@gmail.com>
2024-03-02 01:10:56 +0530

    header colour change

--- assest/header.php
+++ assest/header.php
@@ -1,25 +1,24 @@
-<!-- <header class="blog-header border-bottom shadow-sm"> -->
 <header class="blog-header bhead">
     <link rel="stylesheet" href="css/header.css">
     <div class="container-fluid bgclr" style="padding-left: 3rem; padding-right:3rem">
         <div class="d-flex flex-column flex-md-row align-items-center py-2">
             <a href="index.php" class="my-0 mr-md-auto" style="width: 6rem;">
                 <!-- <img src="img/logo/logo.png" alt="dev culture logo" style="width: 80%;height: auto;"> -->
-                <h3 style="margin-bottom:0px;">InkSpark</h3>
+                <h3 class="txtcle" style="margin-bottom:0px;">InkSpark</h3> <!-- Added txtcle class -->
             </a>
 
             <?php if ($loggedin) : ?>
 
                 <nav class="my-2 my-md-0 mr-md-3">
-                    <a class="p-2 px-5 text-muted" href="index.php">Home</a>
-                    <a class="p-2 px-5 text-muted" href="categories.php">Category</a>
-                    <a class="p-2 px-5 text-muted" href="article.php">Article</a>
-                    <a class="p-2 px-5 text-muted" href="author.php">Author</a>
+                    <a class="p-2 px-5 text-white" href="index.php">Home</a>
+                    <a class="p-2 px-5 text-white" href="categories.php">Category</a>
+                    <a class="p-2 px-5 text-white" href="article.php">Article</a>
+                    <a class="p-2 px-5 text-white" href="author.php">Author</a>
                 </nav>
 
             <?php else : ?>
                 <nav class="my-2 my-md-0 mr-md-3">
-                    <a class="p-2 px-5 text-muted" href="articleOfCategory.php">Articles</a>
+                    <a class="p-2 px-5 text-white" href="articleOfCategory.php">Articles</a>
                 </nav>
 
             <?php endif;  ?>
