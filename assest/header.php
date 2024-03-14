6e96bd53d16f9cf2bbcfbf2e8f1aad63f6c13409
Vexy-04 <varunal2003@gmail.com>
2024-03-14 09:10:47 +0530

    no

--- assest/header.php
+++ assest/header.php
@@ -3,32 +3,30 @@
     <div class="container-fluid bgclr" style="padding-left: 3rem; padding-right:3rem">
         <div class="d-flex flex-column flex-md-row align-items-center py-2">
             <a href="index.php" class="my-0 mr-md-auto" style="width: 6rem;">
-                <h3 class="txtcle" style="margin-bottom:0px;">InkSpark</h3>
+                <!-- <img src="img/logo/logo.png" alt="dev culture logo" style="width: 80%;height: auto;"> -->
+                <h3 class="txtcle" style="margin-bottom:0px;">InkSpark</h3> <!-- Added txtcle class -->
             </a>
 
             <?php if ($loggedin): ?>
-                <?php if ($userRole == 'admin'): ?>
-                    <nav class="my-2 my-md-0 mr-md-3">
+
+                <nav class="my-2 my-md-0 mr-md-3">
+                    <a class="p-2 px-5 text-white" href="index.php">Home</a>
                     <a class="p-2 px-5 text-white" href="categories.php">Category</a>
                     <a class="p-2 px-5 text-white" href="article.php">Article</a>
                     <a class="p-2 px-5 text-white" href="author.php">Author</a>
-                    </nav>
-                <?php elseif ($userRole == 'author'): ?>
-                    <nav class="my-2 my-md-0 mr-md-3">
-                    <a class="p-2 px-5 text-white" href="article.php">Article</a>
-                    </nav>
-                <?php endif; ?>
+                </nav>
 
             <?php else: ?>
                 <nav class="my-2 my-md-0 mr-md-3">
                     <a class="p-2 px-5 text-white" href="articleOfCategory.php">Articles</a>
                     <a class="btn dbtn" href="signup.php">Sign up</a>
                 </nav>
-            <?php endif; ?>
 
+            <?php endif; ?>
             <a class="btn dbtn" href="<?= ($loggedin) ? 'Logout.php' : 'login.php'; ?>">
                 <?= ($loggedin) ? 'Logout' : 'Log in'; ?>
             </a>
+
         </div>
     </div>
 </header>
